6671940e2265415e4cf0469e95af3bdad1ef0e47
swik999 <76035329+swik999@users.noreply.github.com>
2021-02-23 16:11:50 +0700

    Update lab18_2.cpp

--- lab18_2.cpp
+++ lab18_2.cpp
@@ -5,8 +5,12 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
-
-
-
+double overlap(Rect *X,Rect *Y){
+	double O;
+	O = (max(X->x,Y->x)-min((X->x+X->w),(Y->x+Y->w))) * (max((X->y-X->h),(Y->y-Y->h))-min(X->y,Y->y));
+	if(O>0){
+		return O;
+	}else{
+		return 0;
+	}
 }
